0c07ef86d35122289319cbfd696c130c3b5e4cb1
yudarw <yudarismawahyudi@gmail.com>
2023-02-10 16:04:51 +0700

    ...

--- vision_sensor_streaming.py
+++ vision_sensor_streaming.py
@@ -13,19 +13,20 @@ time.sleep(2)
 #es, image = camera.getImage()
 #print(res)
 
-cap = cv.VideoCapture(0)
+#cap = cv.VideoCapture(0)
 
 while sim.simxGetConnectionId(mSim.clientId != -1):
-    #ret, res, image = camera.getImage()    
-    #if ret == sim.simx_return_ok:
-    #    img = np.array(image, dtype=np.uint8)
-    #    img.resize([res[1], res[0], 3])
-    #    img2 = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-    #    img2 = cv.flip(img2, 0)
-    #    cv.imshow("image", img2)    
+    ret, res, image = camera.getImage()    
+    if ret == sim.simx_return_ok:
+        img = np.array(image, dtype=np.uint8)
+        img.resize([res[1], res[0], 3])
+        img2 = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+        img2 = cv.flip(img2, 0)
+        cv.imshow("image", img2)    
     
-    _, frame = cap.read()
-    cv.imshow('frame',frame)
+
+    #_, frame = cap.read()
+    #cv.imshow('frame',frame)
 
     key = cv.waitKey(5) & 0xFF
     if key == 27:
